7e67c5a88aff7194913ac9d69b55c157428079da
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-07-24 20:43:13 -0600

    requiring Letter.js in index.js successful

--- Letter.js
+++ Letter.js
@@ -1,50 +1,30 @@
-//npm init -y — this will initialize a package.json file for your project. The package.json file is required for installing third party npm packages and saving their version numbers
-// dependency for inquirer npm package
-let inquirer = require("inquirer");
-
 let word = "candy";
 let len = word.length;
-let empty = "_____"
+let guessingWord = [];
 
-inquirer.prompt([
-    {
-    name: "underlyingCharacter",
-    message: "Guess a Letter"
-    }
-]).then(function(answers){
-    let guessedLetterBoolean = true;
-    let newLetter = new Letter(answers.underlyingCharacter,guessedLetterBoolean);
-    newLetter.guessed();
+for (let i = 0; i < len; i++) {
+    guessingWord[i] = "_";
+}
 
-})
-
-let Letter = function(underlyingCharacter,guessedLetterBoolean){
-    this.underlyingCharacter = underlyingCharacter;
-    this.guessedLetterBoolean = guessedLetterBoolean;
-    //returns the underlying character if the letter 
+let Letter = function (guessedCharacter) {
+    this.guessedCharacter = guessedCharacter;
+    //this.guessedLetterBoolean = guessedLetterBoolean;
+    //returns the guessed character if the letter 
     //has been guessed, or a placeholder (like an underscore) if the 
     //letter has not been guessed
-    this.guessed = function (underlyingCharacter) {
-        if (this.guessedLetterBoolean === true) {
-            for (let i = 0; i < len; i++) {
-                if (underlyingCharacter === word.charAt(i)) {
-                    empty.charAt(i) = underlyingCharacter;
-                }
-
+    this.guessed = function () {
+        for (let i = 0; i < len; i++) {
+            if (guessedCharacter === word.charAt(i)) {
+                guessingWord[i] = guessedCharacter;
             }
-            console.log(empty);
         }
+        console.log(guessingWord);
         //return character if guessed
     }
 
 }
-    //takes a character as an argument and checks it against the 
-    //underlying character, updating the stored boolean value to 
-    //true if it was guessed correctly
-    this.characterCheckUnderlyingCharacter = function (underlyingCharacter){
-        
-    }
-
+   
+module.exports = Letter;
 
 
 
---
+++ index.js
@@ -0,0 +1,16 @@
+//npm init -y — this will initialize a package.json file for your project. The package.json file is required for installing third party npm packages and saving their version numbers
+// dependency for inquirer npm package
+let Letter = require("./Letter");
+let inquirer = require("inquirer");
+
+inquirer.prompt([
+    {
+    name: "guessedCharacter",
+    message: "Guess a Letter"
+    }
+]).then(function(answers){
+    let newLetter = new Letter(answers.guessedCharacter);
+    newLetter.guessed();
+})
+
+
